d6666c4ca62e578190867f8542d6c82a2bf2b04c
Иван Шмотьев <shmotev.ivan@list.ru>
2023-10-06 09:45:52 +0500

    #1kod

--- PictureViewer2/Form1.cs
+++ PictureViewer2/Form1.cs
@@ -13,9 +13,29 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",","k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
     }
 }
