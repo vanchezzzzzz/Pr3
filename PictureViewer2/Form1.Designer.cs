0812ad3fc93a7016ae74b15e58bcf07e00d0516d
Gbvy Bvgj <mnbg20033@gmail.com>
2023-10-18 09:50:14 +0500

    фыввфыапфа

--- PictureViewer2/Form1.Designer.cs
+++ PictureViewer2/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label16 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
@@ -45,6 +46,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -276,6 +278,10 @@
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -284,7 +290,6 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Matching Game";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -309,6 +314,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PictureViewer2/Form1.cs
+++ PictureViewer2/Form1.cs
@@ -13,12 +13,16 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        //Задача рандомных символов в строку (фигурки)
         Random random = new Random();
         List<string> icons = new List<string>()
         {
             "!", "!", "N", "N", ",", ",","k", "k",
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
+        //firstClicked - первое нажатие игрока, second - второе, изначально равны нулю
+        Label firstClicked = null;
+        Label secondClicked = null;
 
         public Form1()
         {
@@ -27,6 +31,7 @@ namespace MatchingGame
         }
         private void AssignIconsToSquares()
         {
+            //Проверка наличия фигурок на панели и их скрытие от игрока
             foreach (Control control in tableLayoutPanel1.Controls)
             {
                 Label iconLabel = control as Label;
@@ -42,18 +47,64 @@ namespace MatchingGame
 
         private void label1_Click(object sender, EventArgs e)
         {
+            //Включение таймера на исчезновение фигурок и отключение только после двух не совпадающих фигурок
+            if (timer1.Enabled == true)
+                return;
             Label clickedLabel = sender as Label;
             if(clickedLabel !=null)
             {
+                //Если игрок нажал на открытую фигурку то ничего не происходит
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
-                clickedLabel.ForeColor = Color.Black;
+                //Если фигурка не совпадает то меняется цвет на черный и повторяется цикл
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+                //Если игрок выбирает правильные 2 фигурки то они остаются и закрашиваются в черный цвет
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+                //Проверка на победителя
+                CheckForWinner();
+                /*Если игрок выбрал 2 одинаковые фигурки то они сохраняются и сбрасываются на значение null
+                что бы игрок мог дальше играть*/
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+                //Старт таймера на 100 МС
+                timer1.Start();
             }
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-
+            timer1.Stop();
+            //Скрыть обе иконки
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            //Сбросить обе иконки
+            firstClicked = null;
+            secondClicked = null;
+        }
+        private void CheckForWinner()
+        {
+            //Проверка всех фигурок
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+            //Сообщение победителю
+            MessageBox.Show("Молодец!");
+            Close();
         }
     }
 }
