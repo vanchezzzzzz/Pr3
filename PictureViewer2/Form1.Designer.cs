aeed0e719c861e9efb67992ebf5f9cc7f4b59dea
Иван Шмотьев <shmotev.ivan@list.ru>
2023-10-31 14:26:15 +0500

    №1

--- PictureViewer2/Form1.Designer.cs
+++ PictureViewer2/Form1.Designer.cs
@@ -28,34 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chield = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.krokodial = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chield)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.krokodial)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // chield
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 110);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.chield.Image = ((System.Drawing.Image)(resources.GetObject("chield.Image")));
+            this.chield.Location = new System.Drawing.Point(466, 28);
+            this.chield.Name = "chield";
+            this.chield.Size = new System.Drawing.Size(322, 255);
+            this.chield.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.chield.TabIndex = 0;
+            this.chield.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(152, 289);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(247, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Крокодила не тревожить!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(429, 289);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // krokodial
+            // 
+            this.krokodial.Image = ((System.Drawing.Image)(resources.GetObject("krokodial.Image")));
+            this.krokodial.Location = new System.Drawing.Point(12, 84);
+            this.krokodial.Name = "krokodial";
+            this.krokodial.Size = new System.Drawing.Size(204, 143);
+            this.krokodial.TabIndex = 3;
+            this.krokodial.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.krokodial);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.chield);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Крокодильчик";
+            ((System.ComponentModel.ISupportInitialize)(this.chield)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.krokodial)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox chield;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.PictureBox krokodial;
     }
 }
 
--- PictureViewer2/Form1.cs
+++ PictureViewer2/Form1.cs
@@ -16,5 +16,41 @@ namespace PictureViewer2
         {
             InitializeComponent();
         }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            krokodial.Left = krokodial.Left + 20;
+            if (krokodial.Left + krokodial.Width >= this.Width)
+            {
+                tmr.Stop();
+                btnStart.Text = "Детей съели :( ";
+                return;
+            }
+        }
+
+        private bool isAnimating = false;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (isAnimating)
+            {
+                //возвращает изображение в начальное положение
+                //(устанавливает значение свойства Left изображения pct в 0 пикселей)
+                tmr.Stop();
+                krokodial.Left = 0;
+                btnStart.Text = "Вы спасли детей!";
+                isAnimating = false;
+            }
+            else
+            {
+                tmr.Start();
+                btnStart.Text = "Спасти детей!";
+                //Устанавливает значение переменной(выполняется)
+                isAnimating = true;
+            }
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
