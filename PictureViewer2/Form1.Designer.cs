a609d999a37c60ad78b27b5481f2ff8a814dad4a
Gbvy Bvgj <mnbg20033@gmail.com>
2023-11-10 14:27:10 +0500

    #ss

--- PictureViewer2/Form1.Designer.cs
+++ PictureViewer2/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.file1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Location = new System.Drawing.Point(100, 47);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(174, 171);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(355, 47);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(92, 60);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Открыть изображение";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // file1
+            // 
+            this.file1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.OpenFileDialog file1;
     }
 }
 
--- PictureViewer2/Form1.cs
+++ PictureViewer2/Form1.cs
@@ -16,5 +16,18 @@ namespace PictureViewer2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            file1.Filter = "(*.jpg)|*.jpg";
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string fname;
+            file1.ShowDialog();
+            fname = file1.FileName;
+            pct.Image = Image.FromFile(fname);
+        }
     }
 }
